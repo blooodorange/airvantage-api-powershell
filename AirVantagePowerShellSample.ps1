668136cab036caf15a392b7c77de6338aab4ff7a
Martin Walder <martin.walder@me.com>
2017-06-23 10:11:10 +0200

    Add version 1.2

--- AirVantagePowerShellSample.ps1
+++ AirVantagePowerShellSample.ps1
@@ -1,6 +1,6 @@
 ﻿# AirVantagePowerShellSample.ps1
 # Created by Martin Walder | martin ( at ) waldma.org
-# Version 1.1
+# Version 1.2
 # https://doc.airvantage.net/av/howto/cloud/gettingstarted_api/
 # https://doc.airvantage.net/av/reference/cloud/API/
 
@@ -17,6 +17,13 @@ $AVDatacenter = "https://na.airvantage.net"
 
 $AVClientId = ""
 $AVSecretKey = ""
+
+# Prerequisites: existing report with desired DataIDs (report <-> firmware dependency!)
+# Use Get-AirVantageReports to get correct DataIDs
+# Get-AirVantageReports -AVDatacenter $AVDatacenter -AVAccessToken $AVAccessToken -> Configuration
+
+$AVSystemDataID_DeviceUptime = "lwm2m.10250.0.272" # -> Revision 4.8.0.021
+$AVSystemDataID_CurrentWANTimeinUse = "lwm2m.10250.0.5046" # -> Revision 4.8.0.021
 
 # Function Connect-AirVantage - OAUTH authorization code flow
 
@@ -61,6 +68,91 @@ Function Disconnect-AirVantage(){
     }
 }
 
+# Function Get-AirVantageReports
+
+Function Get-AirVantageReports(){
+    param(
+        [Parameter(Mandatory=$true)]
+        $AVDatacenter,
+        [Parameter(Mandatory=$true)]
+        $AVAccessToken
+    )
+    $AVReports = ((Invoke-WebRequest -Method Get -Uri "$AVDatacenter/api/v1/datasets?fields=uid,name,configuration&access_token=$AVAccessToken").Content | ConvertFrom-Json).items | Select @{Name="Name";Expression={$_.name}}, @{Name="UID";Expression={$_.uid}}, @{Name="Configuration";Expression={$_.configuration}} | fl
+    Write-Host -ForegroundColor Yellow "Available reports:"
+    return $AVReports
+}
+
+# Function Retrieve-AirVantageSystemData
+
+Function Retrieve-AirVantageSystemData(){
+    param(
+        [Parameter(Mandatory=$true)]
+        $AVDatacenter,
+        [Parameter(Mandatory=$true)]
+        $AVSystemUID,
+        [Parameter(Mandatory=$true)]
+        $AVSystemDataID,
+        [Parameter(Mandatory=$true)]
+        $AVAccessToken
+    )
+    $Parameters = [ordered]@{
+        systems = @{
+            uids = @(
+                $AVSystemUID
+            )
+        }
+        data = @(
+            $AVSystemDataID
+        )
+    }
+    $JSON = $Parameters | ConvertTo-Json
+    $AVOperation = Invoke-WebRequest -Method Post -Uri "$AVDatacenter/api/v1/operations/systems/data/retrieve?access_token=$AVAccessToken" -ContentType "application/json" -Body $JSON | ConvertFrom-Json
+    Write-Host -ForegroundColor Yellow "Retrieving data..."
+    return $AVOperation
+}
+
+# Function List-AirVantageSystemData
+
+Function List-AirVantageSystemData(){
+    param(
+        [Parameter(Mandatory=$true)]
+        $AVDatacenter,
+        [Parameter(Mandatory=$true)]
+        $AVSystemUID,
+        [Parameter(Mandatory=$true)]
+        $AVSystemDataID,
+        [Parameter(Mandatory=$true)]
+        $AVAccessToken
+    )
+    $AVSystemData = (Invoke-WebRequest -Method Get -Uri "$AVDatacenter/api/v1/systems/$AVSystemUID/data?ids=$AVSystemDataID&access_token=$AVAccessToken").Content | ConvertFrom-Json
+    return $AVSystemData.$AVSystemDataID
+}
+
+# Function Get-AirVantageOperation
+
+Function Get-AirVantageOperation(){
+    param(
+        [Parameter(Mandatory=$true)]
+        $AVDatacenter,
+        [Parameter(Mandatory=$true)]
+        $AVOperationUID,
+        [Parameter(Mandatory=$true)]
+        $AVAccessToken
+    )
+    (Invoke-WebRequest -Method Get -Uri "$AVDatacenter/api/v1/operations/$($AVOperationUID)?access_token=$AVAccessToken").Content | ConvertFrom-Json | Select @{Name="Status";Expression={$_.state}}
+
+}
+
+# Function ConvertFrom-UNIXTimestamp
+
+Function ConvertFrom-UNIXTimestamp(){
+    param(
+        [Parameter(Mandatory=$true)]
+        $Timestamp
+    )
+    [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($Timestamp /1000))
+}
+
 # Function List-AirVantageSystems
 
 Function List-AirVantageSystems(){
@@ -70,7 +162,8 @@ Function List-AirVantageSystems(){
         [Parameter(Mandatory=$true)]
         $AVAccessToken
     )
-    (((Invoke-WebRequest -Method Get -Uri "$AVDatacenter/api/v1/systems?access_token=$AVAccessToken").Content | ConvertFrom-Json).items).name
+    $AVSystems += (((Invoke-WebRequest -Method Get -Uri "$AVDatacenter/api/v1/systems?access_token=$AVAccessToken").Content | ConvertFrom-Json).items).name
+    return $AVSystems
 }
 
 # Function Get-AirVantageSystem
@@ -87,26 +180,39 @@ Function Get-AirVantageSystem(){
     ((Invoke-WebRequest -Method Get -Uri "$AVDatacenter/api/v1/systems?name=$($AVSystemName)&access_token=$AVAccessToken").Content | ConvertFrom-Json).items
 }
 
-# Function ConvertFrom-UNIXTimestamp
-
-Function ConvertFrom-UNIXTimestamp(){
-    param(
-        [Parameter(Mandatory=$true)]
-        $Timestamp
-    )
-    [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($Timestamp /1000))
-}
-
 $AVAccessToken = Connect-AirVantage -AVDatacenter $AVDatacenter -AVUsername $AVUsername -AVPassword $AVPassword -AVClientId $AVClientId -AVSecretKey $AVSecretKey
 
 Write-Host -ForegroundColor Yellow "Registered systems:"
 List-AirVantageSystems -AVDatacenter $AVDatacenter -AVAccessToken $AVAccessToken
 
-$AVSystemName = Read-Host "`nEnter system's name (not case sensitive)"
+$AVSystemName = Read-Host "Enter system's name (not case sensitive)"
 $PingCount = Read-Host "Enter the number of pings you want to send to the gateway"
 
 Write-Host -ForegroundColor Yellow "Collecting device information..."
 $AVOutput = Get-AirVantageSystem -AVDatacenter $AVDatacenter -AVSystemName $AVSystemName -AVAccessToken $AVAccessToken
+
+$AVSystemUID = $AVOutput.uid
+
+$AVOperationUID_CurrentWANTimeinUse = (Retrieve-AirVantageSystemData -AVDatacenter $AVDatacenter -AVSystemUID $AVSystemUID -AVSystemDataID $AVSystemDataID_CurrentWANTimeinUse -AVAccessToken $AVAccessToken).operation
+
+do {
+    Get-AirVantageOperation -AVDatacenter $AVDatacenter -AVOperationUID $AVOperationUID_CurrentWANTimeinUse -AVAccessToken $AVAccessToken
+}
+until ((Get-AirVantageOperation -AVDatacenter $AVDatacenter -AVOperationUID $AVOperationUID_CurrentWANTimeinUse -AVAccessToken $AVAccessToken).Status -eq "FINISHED")
+Write-Host -ForegroundColor Yellow "Operation finished"
+
+$AVOperationUID_DeviceUptime = (Retrieve-AirVantageSystemData -AVDatacenter $AVDatacenter -AVSystemUID $AVSystemUID -AVSystemDataID $AVSystemDataID_DeviceUptime -AVAccessToken $AVAccessToken).operation
+
+do {
+    Get-AirVantageOperation -AVDatacenter $AVDatacenter -AVOperationUID $AVOperationUID_DeviceUptime -AVAccessToken $AVAccessToken
+}
+until ((Get-AirVantageOperation -AVDatacenter $AVDatacenter -AVOperationUID $AVOperationUID_DeviceUptime -AVAccessToken $AVAccessToken).Status -eq "FINISHED")
+Write-Host -ForegroundColor Yellow "Operation finished"
+
+Start-Sleep -Seconds 5 # Waiting on data to be present in AirVantage
+
+$CurrentWANTimeinUse = List-AirVantageSystemData -AVDatacenter $AVDatacenter -AVSystemUID $AVSystemUID -AVSystemDataID $AVSystemDataID_CurrentWANTimeinUse -AVAccessToken $AVAccessToken
+$DeviceUptime = List-AirVantageSystemData -AVDatacenter $AVDatacenter -AVSystemUID $AVSystemUID -AVSystemDataID $AVSystemDataID_DeviceUptime -AVAccessToken $AVAccessToken
 
 if ($AVOutput -ne $null){
 
@@ -139,6 +245,8 @@ if ($AVOutput -ne $null){
     $SystemInfo | Add-Member -NotePropertyName "Board temperature" -NotePropertyValue $AVOutput.data.boardTemp
     $SystemInfo | Add-Member -NotePropertyName "Radio module temperature" -NotePropertyValue $AVOutput.data.radioModuleTemp
     $SystemInfo | Add-Member -NotePropertyName "Number of resets" -NotePropertyValue $AVOutput.data.numberofResets
+    $SystemInfo | Add-Member -NotePropertyName "Current WAN time in use (d.hh:mm:ss)" -NotePropertyValue (New-TimeSpan -Minutes ($CurrentWANTimeinUse).value)
+    $SystemInfo | Add-Member -NotePropertyName "Device uptime" -NotePropertyValue ($DeviceUptime).value
 
     Write-Host -ForegroundColor Yellow "Device info:"
     $SystemInfo | fl
--- README.md
+++ README.md
@@ -4,5 +4,7 @@ An example project of how to use AirVantage API with PowerShell 5:
 * Requests an OAUTH token
 * Lists all registered systems
 * Collects information on a specific system
+* Retrieves data on a specific system (defined via DataIDs configured in a report)
+* Outputs information/data
 * Tries to ping gateway's WAN IP from local machine and shows stats
 * Expires the OAUTH token
